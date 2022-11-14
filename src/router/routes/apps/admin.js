a9a74d0ce303324bf08605aa67cc72f1a7bcd085
hishamudin98 <hishamudin.ali@gmail.com>
2022-11-15 04:16:43 +0800

    changes

--- src/router/routes/apps/admin.js
+++ src/router/routes/apps/admin.js
@@ -1,63 +1,67 @@
-export default [
-    {
-      path: "/admin/login",
-      name: "login",
-      component: () => import("@/views/apps/administrator/adminLogin.vue"),
-    },
-     {
-      path: "/admin/:staffid?",
-      name: "dashboard",
-      component: () => import("@/views/apps/administrator/adminDashboard.vue"),
+export default [{
+        path: "/admin/login",
+        name: "login",
+        component: () =>
+            import ("@/views/apps/administrator/adminLogin.vue"),
     },
     {
-      path: "/admin/user",
-      name: "admin-user",
-      component: () => import("@/views/apps/administrator/Membership/adminUser.vue"),
+        path: "/admin/:staffid?",
+        name: "dashboard",
+        component: () =>
+            import ("@/views/apps/administrator/adminDashboard.vue"),
     },
     {
-      path: "/admin/staff",
-      name: "admin-staff",
-      component: () => import("@/views/apps/administrator/User/adminStaff.vue"),
+        path: "/admin/user",
+        name: "admin-user",
+        component: () =>
+            import ("@/views/apps/administrator/Membership/adminUser.vue"),
     },
     {
-      path: "/admin/menu",
-      name: "admin-menu",
-      component: () => import("@/views/apps/administrator/Menu/adminMenu.vue"),
+        path: "/admin/staff",
+        name: "admin-staff",
+        component: () =>
+            import ("@/views/apps/administrator/User/adminStaff.vue"),
     },
     {
-      path: "/admin/report/transaction",
-      name: "admin-report-t",
-      component: () => import("@/views/apps/administrator/Reports/adminReports.vue"),
+        path: "/admin/menu",
+        name: "admin-menu",
+        component: () =>
+            import ("@/views/apps/administrator/Menu/adminMenu.vue"),
     },
     {
-      path: "/admin/report/shift",
-      name: "admin-report-s",
-      component: () => import("@/views/apps/administrator/Reports/adminReportsShift.vue"),
+        path: "/admin/report/transaction",
+        name: "admin-report-t",
+        component: () =>
+            import ("@/views/apps/administrator/Reports/adminReports.vue"),
     },
     {
-      path: "/admin/report/refund",
-      name: "admin-report-r",
-      component: () => import("@/views/apps/administrator/Reports/adminReportsRefund.vue"),
+        path: "/admin/report/shift",
+        name: "admin-report-s",
+        component: () =>
+            import ("@/views/apps/administrator/Reports/adminReportsShift.vue"),
     },
     {
-      path: "/admin/table",
-      name: "admin-table",
-      component: () => import("@/views/apps/administrator/Outlet/adminTable.vue"),
+        path: "/admin/report/refund",
+        name: "admin-report-r",
+        component: () =>
+            import ("@/views/apps/administrator/Reports/adminReportsRefund.vue"),
     },
     {
-      path: "/admin/order",
-      name: "admin-order",
-      component: () => import("@/views/apps/administrator/Order/adminOrder.vue"),
+        path: "/admin/table",
+        name: "admin-table",
+        component: () =>
+            import ("@/views/apps/administrator/Outlet/adminTable.vue"),
     },
     {
-      path: "/admin/outlet",
-      name: "admin-outlet",
-      component: () => import("@/views/apps/administrator/Outlet/adminOutlet.vue"),
+        path: "/admin/order",
+        name: "admin-order",
+        component: () =>
+            import ("@/views/apps/administrator/adminOrder.vue"),
     },
+
     /*{
-      path: "/order/previous/:orderID?",
-      name: "order-previous",
-      component: () => import("@/views/apps/order/OrderPrevious.vue"),
-    }, */
-  ];
-  
+                path: "/order/previous/:orderID?",
+                name: "order-previous",
+                component: () => import("@/views/apps/order/OrderPrevious.vue"),
+              }, */
+];
